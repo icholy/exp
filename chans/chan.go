cc2b55366bcd206637225fea26f8bd19881eab13
Ilia Choly <ilia.choly@gmail.com>
2023-02-10 11:56:12 -0500

    move Go logic to Chan method

--- chans/chan.go
+++ chans/chan.go
@@ -18,31 +18,17 @@ func (c Chan[T]) Recv() (T, error) {
 	return r.Value, r.Err
 }
 
-func Go[T any](g *errgroup.Group, f func() (T, error)) Chan[T] {
-	ch := make(Chan[T], 1)
-	if g == nil {
-		go func() {
-			var r Result[T]
-			r.Value, r.Err = f()
-			ch <- r
-		}()
-	} else {
-		g.Go(func() error {
-			var r Result[T]
-			r.Value, r.Err = f()
-			ch <- r
-			return r.Err
-		})
-	}
-	return ch
+func (c Chan[T]) Go(g *errgroup.Group, f func() (T, error)) {
+	g.Go(func() error {
+		var r Result[T]
+		r.Value, r.Err = f()
+		ch <- r
+		return r.Err
+	})
 }
 
-func Recv[T any](ctx context.Context, ch Chan[T]) (T, error) {
-	select {
-	case r := <-ch:
-		return r.Value, r.Err
-	case <-ctx.Done():
-		var z T
-		return z, ctx.Err()
-	}
+func Go[T any](g *errgroup.Group, f func() (T, error)) Chan[T] {
+	ch := make(Chan[T], 1)
+	ch.Go(g, f)
+	return ch
 }
